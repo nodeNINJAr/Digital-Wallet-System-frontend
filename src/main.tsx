69db550d8586630462d066242d2b62e92d93aba6
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-09-22 17:47:58 +0600

    redux-setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './routes/index.ts'
 import { ThemeProvider } from './providers/theme-provider.tsx'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.ts'
 
 
 
@@ -12,8 +14,10 @@ import { ThemeProvider } from './providers/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <RouterProvider router={router} />
-    </ThemeProvider>
+     <Provider store={store}>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <RouterProvider router={router} />
+        </ThemeProvider>
+    </Provider>
   </StrictMode>
 )
